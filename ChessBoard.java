ce54f1a03ff22338d8e691ccb323f74d27a1897d
MASSIVENAVE <evangmarriott@gmail.com>
2022-01-12 13:07:05 -0800

    Update ChessBoard.java
    made parse move method and swapping pieces

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,10 +1,56 @@
 import java.util.*;
 //Kevin was here
+//so was Evan
 public class ChessBoard{
     ArrayList<ArrayList<Character>> board;
     ChessRules obj;
     String move;
-
+    
+    public String parseMove(String move) { 
+        String col_index = "";
+        String row_index = "";
+        String col = move.substring(0,1);
+        String row = move.substring(1);
+        
+        if (col.equals("a")) {
+            col_index = "1";
+        } else if (col.equals("b")) {
+            col_index = "2";
+        } else if (col.equals("c")) {
+            col_index = "3";
+        } else if (col.equals("d")) {
+            col_index = "4";
+        } else if (col.equals("e")) {
+            col_index = "5";
+        } else if (col.equals("f")) {
+            col_index = "6";
+        } else if (col.equals("g")) {
+            col_index = "7";
+        } else if (col.equals("h")) {
+            col_index = "8";
+        }
+        
+        if (row.equals("1")) {
+            row_index = "7";
+        } else if (row.equals("2")) {
+            row_index = "6";
+        } else if (row.equals("3")) {
+            row_index = "5";
+        } else if (row.equals("4")) {
+            row_index = "4";
+        } else if (row.equals("5")) {
+            row_index = "3";
+        } else if (row.equals("6")) {
+            row_index = "2";
+        } else if (row.equals("7")) {
+            row_index = "1";
+        } else if (row.equals("8")) {
+            row_index = "0";
+        }
+        
+        return row_index + "," + col_index;
+    } // will return the col of the imputted move, ex. a7 will give 1,1
+    
     public void coolPrint(String str){
         for(int i = 0; i < str.length(); i++){
             try{
@@ -40,7 +86,7 @@ public class ChessBoard{
         for(int i = 0; i < 9; i++){
             ArrayList<Character> x = new ArrayList<Character>();
             for(int j = 0; j < 9; j++){
-                x.add('☐');
+                x.add('⏣');
             }
             board.add(x);
         }
@@ -92,7 +138,27 @@ public class ChessBoard{
         printBoard();
 
         //obj = new ChessRules(board);
-        //playGame();
+        playGame();
+    }
+    
+    public void movePiece(ArrayList<ArrayList<ChessPiece>> board, String move) {
+        // first check if possible
+        String start = move.substring(0,2);
+        String end = move.substring(2);
+        
+        String start_row_str = parseMove(start).substring(0,1);
+        String start_col_str = parseMove(start).substring(2);
+        String end_row_str = parseMove(end).substring(0,1);
+        String end_col_str = parseMove(end).substring(2);
+        int start_row = Integer.parseInt(start_row_str);
+        int start_col = Integer.parseInt(start_col_str);
+        int end_row = Integer.parseInt(end_row_str);
+        int end_col = Integer.parseInt(end_col_str);
+        
+        ChessPiece temp = board.get(end_row).get(end_col);
+        
+        board.get(end_row).set(end_col, board.get(start_row).get(start_col));
+        board.get(start_row).set(start_col, temp);
     }
 
     public void playGame(){
