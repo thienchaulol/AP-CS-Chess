c26b0e20f4eedac7de283aeed1ca73697ff93453
Thien Chau <thienchau010@gmail.com>
2022-01-12 12:33:12 -0800

    Board now uses ChessPiece objects rather then Characters

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,7 +1,7 @@
 import java.util.*;
 //Kevin was here
 public class ChessBoard{
-    ArrayList<ArrayList<Character>> board;
+    ArrayList<ArrayList<ChessPiece>> board;
     ChessRules obj;
     String move;
 
@@ -20,14 +20,14 @@ public class ChessBoard{
     public void printBoard(){
         for(int i = 0; i < board.size()-1; i++){
             for(int j = 0; j < board.get(i).size(); j++){
-                System.out.print(board.get(i).get(j) + " ");
+                System.out.print(board.get(i).get(j).name + " ");
             }
             System.out.println();
         }
 
         for(int i = 0; i < board.get(8).size(); i++){
             if(i == 0) System.out.print("- ");
-            else System.out.print(board.get(8).get(i) + " ");
+            else System.out.print(board.get(8).get(i).name + " ");
             if(i == 1) System.out.print(" ");
             if(i == 2) System.out.print(" ");
             if(i == 4) System.out.print(" ");
@@ -38,48 +38,49 @@ public class ChessBoard{
 
     public void createStartingBoard(){
         for(int i = 0; i < 9; i++){
-            ArrayList<Character> x = new ArrayList<Character>();
+            ArrayList<ChessPiece> x = new ArrayList<ChessPiece>();
             for(int j = 0; j < 9; j++){
-                x.add('☐');
+                x.add(new ChessPiece("☐", false));
             }
             board.add(x);
         }
 
         char start = 'a'; //populate bottom letters
         for(int i = 1; i < 9; i++){
-            board.get(8).set(i, start++);
+            
+            board.get(8).set(i, new ChessPiece(String.valueOf(start++), false));
         }
 
         for(int i = 1; i < 9; i++){
-            board.get(8-i).set(0, (char)(i+'0'));
+            board.get(8-i).set(0, new ChessPiece(String.valueOf((char)(i+'0')),false));
         }
 
         //Row 1 and Row 6 are 'P's (pawns)
         for(int i = 1; i < 9; i++){
-            board.get(1).set(i, '♟');
-            board.get(6).set(i, '♙');
+            board.get(1).set(i, new ChessPiece("♟", true));
+            board.get(6).set(i, new ChessPiece("♙", true));
         }
 
         //Corners are Rooks
-        board.get(0).set(1, '♜');
-        board.get(0).set(8, '♜');
-        board.get(7).set(1, '♖');
-        board.get(7).set(8, '♖');
+        board.get(0).set(1, new ChessPiece("♜", true));
+        board.get(0).set(8, new ChessPiece("♜", true));
+        board.get(7).set(1, new ChessPiece("♖", false));
+        board.get(7).set(8, new ChessPiece("♖", false));
         //Next to corners are Knights
-        board.get(0).set(2, '♞');
-        board.get(0).set(7, '♞');
-        board.get(7).set(2, '♘');
-        board.get(7).set(7, '♘');
+        board.get(0).set(2, new ChessPiece("♞", true));
+        board.get(0).set(7, new ChessPiece("♞", true));
+        board.get(7).set(2, new ChessPiece("♘", false));
+        board.get(7).set(7, new ChessPiece("♘", false));
         //Next to Knights are Bishops
-        board.get(0).set(3, '♝');
-        board.get(0).set(6, '♝');
-        board.get(7).set(3, '♗');
-        board.get(7).set(6, '♗');
+        board.get(0).set(3, new ChessPiece("♝", false));
+        board.get(0).set(6, new ChessPiece("♝", false));
+        board.get(7).set(3, new ChessPiece("♗", false));
+        board.get(7).set(6, new ChessPiece("♗", false));
         //White will always be bottom, black on top
-        board.get(0).set(4, '♛');
-        board.get(0).set(5, '♚');
-        board.get(7).set(4, '♕');
-        board.get(7).set(5, '♔');
+        board.get(0).set(4, new ChessPiece("♛", false));
+        board.get(0).set(5, new ChessPiece("♚", false));
+        board.get(7).set(4, new ChessPiece("♕", false));
+        board.get(7).set(5, new ChessPiece("♔", false));
     }
 
     public void populateEmptyBoard(){
