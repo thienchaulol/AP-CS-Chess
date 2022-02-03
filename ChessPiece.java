b0504ad4b0f3e4de6c0052d358916ffd2cfeab0d
KevinTvT <kevin.nie.24@sacredsf.org>
2022-02-03 10:50:40 -0800

    Chess piece has moved

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,6 +1,7 @@
 public class ChessPiece{
     public String name;
     public boolean isB;
+    public boolean hasMoved = false;
     public ChessPiece(String n, boolean val){
         this.name = n;
         this.isB = val;
